0c98fcaaef5a61427cda7618a7c64ffa9243dd03
marcelo-luiz <marcelo_lds01@hotmail.com>
2025-09-15 17:01:47 -0300

    add description to socks

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            description: 'Here is a blue/green socks maded with carefully hands'
+        }
+    }
+})
